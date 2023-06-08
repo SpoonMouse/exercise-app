720b13e2154245466a3e3048e0db6f47c813a5e4
SpoonMouse <tracyd153@gmail.com>
2023-06-07 20:00:24 -0500

    fixed horizontal scroll version, now at 2.7.0

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,15 +1,19 @@
 import React, { useEffect, useState} from 'react';
 import { Box, Button, Stack, TextField, Typography } from '@mui/material';
 import { exerciseOptions, fetchData } from '../utils/fetchData';
+import HorizontalScrollBar from './HorizontalScrollBar';
 
-const SearchExercises = () => {
+const SearchExercises = ({ setExercises, bodyPart, setBodyPart }) => {
   const [search, setSearch] = useState('');
-  const [exercises, setExercises] = useState([])
+  const [bodyParts, setBodyParts] = useState([]);
 
   useEffect(() => {
     const fetchExercisesData = async () => {
-      const bodyPartsData = await fetchData('https://exercisedb.p.rapidapi.com/exercises/bodyPartList', exerciseOptions)
+      const bodyPartsData = await fetchData('https://exercisedb.p.rapidapi.com/exercises/bodyPartList', exerciseOptions);
+
+      setBodyParts(['all', ...bodyPartsData]);
     }
+      fetchExercisesData();
   }, [])
 
   const handleSearch = async () => {
@@ -67,6 +71,9 @@ const SearchExercises = () => {
         >
             Search
         </Button>
+      </Box>
+      <Box sx={{ position: 'relative', width: '100%', p: '20px'}}>
+          <HorizontalScrollBar data={bodyParts} bodyPart={bodyPart} setBodyPart={setBodyPart}/>
       </Box>
     </Stack>
   )
